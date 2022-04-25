bf19792088899cdba7ad7664495a7e3eea511a4a
Ryan Kim <ryankim0709@gmail.com>
2022-04-25 11:22:39 -0700

    FOOP Classwork - Power Generator

--- Classwork/FOOP/Power_Generator/Generator.java
+++ Classwork/FOOP/Power_Generator/Generator.java
@@ -1,5 +1,17 @@
 package Classwork.FOOP.Power_Generator;
+import java.lang.Math;
 
 public class Generator {
-    
+    private double num;
+    private int raised;
+
+    public Generator(double num) {
+        this.num = num;
+        this.raised = -1;
+    }
+
+    public double next() {
+        raised++;
+        return Math.pow(num, raised);
+    }
 }
--- Classwork/FOOP/Power_Generator/Main.java
+++ Classwork/FOOP/Power_Generator/Main.java
@@ -6,5 +6,11 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("What number would you like to raise");
+        double num = Double.parseDouble(br.readLine());
+
+        Generator generator = new Generator(num);
+        for (int i = 0; i < 10; i++) {
+            System.out.println(generator.next());
+        }
     }
 }

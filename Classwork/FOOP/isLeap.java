c1d1ed554dc323e9b50e93d3680f3ab4bb48cb69
Ryan Kim <ryankim0709@gmail.com>
2022-04-28 14:24:59 -0700

    4/28/22

---
+++ Classwork/FOOP/isLeap.java
@@ -0,0 +1,16 @@
+package Classwork.FOOP;
+import java.io.*;
+
+public class isLeap {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            System.out.println("Enter a year to check if it is a leap year(-1 to quit) ");
+            int year = Integer.parseInt(br.readLine());
+            if (year == -1) {
+                break;
+            }
+            System.out.println((((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) ? "Leap" : "Non-leap");
+        }
+    }
+}

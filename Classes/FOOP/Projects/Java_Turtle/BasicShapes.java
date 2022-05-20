69fe5487aaf3342fd17169c04a0f9bc7981ea14e
Ryan Kim <ryankim0709@gmail.com>
2022-05-19 19:53:21 -0700

    Foop project snowflake

---
+++ Classes/FOOP/Projects/Java_Turtle/BasicShapes.java
@@ -0,0 +1,80 @@
+package Classes.FOOP.Projects.Java_Turtle;
+
+import javax.swing.*;
+import java.awt.event.*;
+import java.util.*;
+import java.awt.*;
+import java.io.*;
+
+public class BasicShapes {
+    static Color penColor;
+    static Turtle turtle;
+    static int colorInd = -1;
+    static String[] colors;
+
+    public static void main(String[] args) throws IOException {
+        World world = new World(1000, 1000);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("How many sides you want on your shapes?");
+        int sides = Integer.parseInt(br.readLine());
+        System.out.println("Please input a list of colors in (R G B) form. ex (225, 50, 100)/(200, 100, 75)");
+        // RGB values can range from 0 to 225
+
+        colors = br.readLine().split("/");
+        double degrees = 360.0 / sides;
+
+        turtle = new Turtle(world);
+        updateColor();
+
+        drawPolygon(degrees);
+        drawPinwheel(degrees);
+        drawAsterics(degrees);
+
+        world.setVisible(true);
+    }
+
+    public static void drawPolygon(double degrees) {
+        turtle.penUp();
+        turtle.moveTo(300, 300);
+        turtle.penDown();
+        for (int i = 0; i < 360 / degrees; i++) {
+            updateColor();
+            turtle.forward(50);
+            turtle.turn(18);
+        }
+    }
+
+    public static void drawPinwheel(double degrees) {
+        turtle.penUp();
+        turtle.moveTo(600, 600);
+        turtle.penDown();
+        for (int i = 0; i < 360 / degrees; i++) {
+            updateColor();
+            turtle.forward(100);
+            turtle.backward(50);
+            turtle.turn(degrees);
+        }
+    }
+
+    public static void drawAsterics(double degrees) {
+        turtle.penUp();
+        turtle.moveTo(200, 800);
+        turtle.penDown();
+        for (int i = 0; i < 360 / degrees; i++) {
+            updateColor();
+            turtle.forward(50);
+            turtle.backward(50);
+            turtle.turn(degrees);
+        }
+    }
+
+    public static void updateColor() {
+        colorInd += 1;
+        colorInd = colorInd % colors.length;
+        String currColor = colors[colorInd];
+        currColor = currColor.replace("(", "");
+        currColor = currColor.replace(")", "");
+        String[] rgb = currColor.split(", ");
+        turtle.setPenColor(new Color(Integer.parseInt(rgb[0]), Integer.parseInt(rgb[1]), Integer.parseInt(rgb[2])));
+    }
+}
--- Classes/FOOP/Projects/Java_Turtle/SnowFlake.java
+++ Classes/FOOP/Projects/Java_Turtle/SnowFlake.java
@@ -6,7 +6,7 @@ import java.util.*;
 import java.awt.*;
 import java.io.*;
 
-public class Main {
+public class SnowFlake {
     static Color penColor;
     static Turtle turtle;
     static int colorInd = -1;
@@ -15,57 +15,55 @@ public class Main {
     public static void main(String[] args) throws IOException {
         World world = new World(1000, 1000);
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("How many sides you want on your shapes?");
+        System.out.println("How many sides you want on your snowflake?");
         int sides = Integer.parseInt(br.readLine());
         System.out.println("Please input a list of colors in (R G B) form. ex (225, 50, 100)/(200, 100, 75)");
         // RGB values can range from 0 to 225
 
         colors = br.readLine().split("/");
-        double degrees = 360.0 / sides;
-
         turtle = new Turtle(world);
-        updateColor();
-
-        drawPolygon(degrees);
-        drawPinwheel(degrees);
-        drawAsterics(degrees);
-
+        drawSnowFlake(sides);
         world.setVisible(true);
     }
-
-    public static void drawPolygon(double degrees) {
-        turtle.penUp();
-        turtle.moveTo(300, 300);
-        turtle.penDown();
-        for (int i = 0; i < 360 / degrees; i++) {
+    
+    public static void drawSnowFlake(int sides) {
+        turtle.moveTo(500, 500);
+        for (int i = 0; i < sides; i++) {
             updateColor();
-            turtle.forward(50);
-            turtle.turn(18);
+            drawSquares();
+            turtle.turn(360.0/(sides+6));
         }
     }
 
-    public static void drawPinwheel(double degrees) {
+    public static void drawSquares() {
+        // Offset
         turtle.penUp();
-        turtle.moveTo(600, 600);
+        turtle.forward(5);
+        turtle.turnRight();
+        turtle.forward(5);
+        turtle.turnLeft();
         turtle.penDown();
-        for (int i = 0; i < 360 / degrees; i++) {
-            updateColor();
-            turtle.forward(100);
-            turtle.backward(50);
-            turtle.turn(degrees);
+
+        // Draw Squares
+        for (int i = 0; i < 4; i++) {
+            drawSquare();
+            turtle.penUp();
+            turtle.forward(10);
+            turtle.penDown();
         }
+
+        // Return Offset
+        turtle.penUp();
+        turtle.moveTo(500, 500);
+        turtle.penDown();
     }
 
-    public static void drawAsterics(double degrees) {
-        turtle.penUp();
-        turtle.moveTo(200, 800);
-        turtle.penDown();
-        for (int i = 0; i < 360 / degrees; i++) {
-            updateColor();
-            turtle.forward(50);
-            turtle.backward(50);
-            turtle.turn(degrees);
+    public static void drawSquare() {
+        for (int i = 0; i < 4; i++) {
+            turtle.forward(300);
+            turtle.turnRight();
         }
+        turtle.turnLeft();
     }
 
     public static void updateColor() {
